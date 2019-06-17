d3e213823aee927a52bd447c22bba752200038a1
victoria <victoria10011001@hotmail.com>
2019-06-17 15:10:17 -0700

    adds inital project files

---
+++ README.md
@@ -0,0 +1,5 @@
+The Astro Weight Calculator was developed to show how much you would weigh on a different planet. In the "Calculate Weight" section, you can put in a certain weight in pounds and then choose the planet you would like to calculate your weight on. When you submit your weight, the site will tell you how much you would weigh if you were on that other planet. The weight multiplier is prepopulated with the default planets in the drop down.
+
+If you would like to add your own planet with a unique weight multiplier, you can do that in the "Imput Custom Planet" section. This adds your custom planet as an item in the drop down menu in the "Calculate Weight" section.
+
+Enjoy!
--- main.js
+++ main.js
@@ -1,1 +1,64 @@
-// Write your JavaScript code here!
+
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+  ];
+
+  planets = planets.reverse();
+  
+  
+  // We're going to solve this by breaking the problem into three parts.
+  // Programmers like automating things, we'll populate the HTML drop down options using code,
+  // instead of having to type out all the values.
+  // Create a function that does the some math and gives us the new weight.
+  // Then create a function that responds when the user clicks on the button.
+  
+  planets.forEach( function(item) { 
+    var o = document.createElement("option");
+    o.text = item[0];
+    o.value = item[0];
+    document.getElementById("planets").appendChild(o);
+ });
+
+
+  function calculateWeight(weight, planetName) {
+    var result;
+    planets.forEach(function(item) {
+      if(item[0] === planetName) {
+        result = weight * item[1]
+      }
+    })
+
+    return result;
+  }
+  
+  function handleClickEvent(e) {
+    var newWeight;
+    var currentWeight = document.getElementById("user-weight").value;
+    var planetName = document.getElementById("planets").value
+    currentWeight = parseInt(currentWeight, 10);
+    if(currentWeight) {
+      newWeight = calculateWeight(currentWeight, planetName);
+    }
+   var output = document.getElementById("output");
+   output.innerText = "If you were on " + planetName + ", you would weigh " + newWeight + "lbs!";
+  
+  }
+
+  document.getElementById("calculate-button").onclick = handleClickEvent
+  
+
+
+  // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
+  // Bonus Challenges
+// 8. Reverse the drop down order so that the sun is first.
+// 9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)
